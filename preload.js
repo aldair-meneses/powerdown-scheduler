beb9e988495020eab4cb654f113d9e452c40f37c
aldair meneses <aldair.meneses@protonmail.com>
2023-04-15 07:06:22 -0300

    Add a simple logic to preload for shutdown

--- preload.js
+++ preload.js
@@ -1,3 +1,8 @@
-window.addEventListener('DOMContentLoaded', () => {
+const { ipcRenderer } = require("electron");
 
+window.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.shutdown');
+    button.addEventListener('click', ()=> {
+        ipcRenderer.send('execute-command', 'shutdown /s /t 60')
+    });
 })
