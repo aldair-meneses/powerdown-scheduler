7b3d06b184ef1c13320c22ac22c7e430edfa2207
aldair meneses <aldair.meneses@protonmail.com>
2023-04-15 07:05:54 -0300

    Add shutdown to main process

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, nativeTheme, ipcMain } = require("electron");
+const {exec} = require("child_process")
+const { stdout, stderr } = require("process");
 const path = require("path");
 
 const createWindow = () => {
@@ -7,18 +9,35 @@ const createWindow = () => {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
-        }
+        },
     });
-
     win.loadFile("index.html");
 }
 
+ipcMain.on('execute-command', (event, command) => {
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log('Vai desligar');
+      console.log(`stdout: ${stdout}`);
+      console.log(`stderr: ${stderr}`);
+    });
+});
+
+
 app.whenReady().then(() => {
     createWindow();
+    nativeTheme.themeSource = 'dark';
 
     app.on('activate', () => {
-        if (BrowserWindow().length === 0) createWindow()
-    })
-})
+        if (BrowserWindow().length === 0) createWindow();
+    });
+});
 
-console.log(path.join(__dirname, 'preload.js'));
+app.on('window-all-closed', ()=> {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
